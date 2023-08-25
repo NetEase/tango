6192928dbf480deb9bfccc51ee305cb3cdd7165b
wwsun <ww.sww@outlook.com>
2023-08-25 17:04:41 +0800

    docs: update

--- apps/website/src/pages/index.tsx
+++ apps/website/src/pages/index.tsx
@@ -6,7 +6,7 @@ import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
-import { Alert } from '../components/alert';
+import { Alert } from '../components/Alert';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
