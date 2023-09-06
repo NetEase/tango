fda2fb971f755651b3250509a083e83f1d9a94da
wwsun <ww.sww@outlook.com>
2023-09-06 19:04:26 +0800

    docs: add product hunt link

--- apps/website/src/pages/index.tsx
+++ apps/website/src/pages/index.tsx
@@ -36,6 +36,18 @@ function HomepageHeader() {
               message: '使用文档',
             })}
           </Link>
+          <a
+            href="https://www.producthunt.com/posts/tango-b8474917-fbce-4180-9fac-4ac7d7a6fa7d?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-tango&#0045;b8474917&#0045;fbce&#0045;4180&#0045;9fac&#0045;4ac7d7a6fa7d"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=412854&theme=light"
+              alt="Tango - A&#0032;source&#0032;code&#0032;based&#0032;low&#0045;code&#0032;builder | Product Hunt"
+              width="250px"
+              height="54px"
+            />
+          </a>
         </div>
         <div className={styles.heroImageBox}>
           <img
@@ -43,6 +55,7 @@ function HomepageHeader() {
             src="https://p6.music.126.net/obj/wonDlsKUwrLClGjCm8Kx/30108642346/b8cf/e86d/ef5a/514d90b722b5d8dc0e18516ed594a07b.png"
             alt="preview"
           />
+          d
         </div>
       </div>
     </header>
