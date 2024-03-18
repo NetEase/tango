9f7441c13400dba55c58b7dd6ce9429013edbc45
Wells <ww.sun@outlook.com>
2024-03-18 16:49:41 +0800

    fix: add deprecated and data group to ComponentPropType (#116)
    * fix: add deprecated and data group to ComponentPropType
    
    * fix: render choiceSetter with icon

--- apps/playground/src/pages/index.tsx
+++ apps/playground/src/pages/index.tsx
@@ -46,7 +46,7 @@ const sandboxQuery = new DndQuery({
 
 // 4. 图标库初始化（物料面板和组件树使用了 iconfont 里的图标）
 createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/c/font_2891794_lzc7rtwuzf.js',
+  scriptUrl: '//at.alicdn.com/t/c/font_2891794_cou9i7556tl.js',
 });
 
 /**
