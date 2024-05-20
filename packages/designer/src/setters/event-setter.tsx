6c0ad4df7372580080efa03dcad5de47dbce1f79
BoBoooooo <17746714@qq.com>
2024-05-20 16:46:17 +0800

    feat: event-setter & expression-setter use popover

--- packages/designer/src/setters/event-setter.tsx
+++ packages/designer/src/setters/event-setter.tsx
@@ -1,11 +1,11 @@
-import React, { useCallback, useState } from 'react';
-import { css, Box } from 'coral-system';
+import React, { useCallback, useMemo, useState } from 'react';
+import { css, Box, Text } from 'coral-system';
 import { AutoComplete } from 'antd';
 import { ActionSelect } from '@music163/tango-ui';
 import { FormItemComponentProps } from '@music163/tango-setting-form';
 import { useWorkspace, useWorkspaceData } from '@music163/tango-context';
 import { wrapCode } from '@music163/tango-helpers';
-import { ExpressionModal } from './expression-setter';
+import { ExpressionPopover } from './expression-setter';
 import { value2code } from '@music163/tango-core';
 
 enum EventAction {
@@ -25,15 +25,6 @@ const wrapperStyle = css`
   }
 `;
 
-const options = [
-  { label: '无动作', value: EventAction.NoAction },
-  { label: '打印事件', value: EventAction.ConsoleLog },
-  { label: '绑定 JS 表达式', value: EventAction.BindExpression },
-  { label: '打开页面', value: EventAction.NavigateTo },
-  { label: '打开弹窗', value: EventAction.OpenModal },
-  { label: '关闭弹窗', value: EventAction.CloseModal },
-];
-
 export type EventSetterProps = FormItemComponentProps<string>;
 
 /**
@@ -43,7 +34,6 @@ export function EventSetter(props: EventSetterProps) {
   const { value, onChange, modalTitle } = props;
   const [type, setType] = useState<EventAction>(); // 事件类型
   const [temp, setTemp] = useState(''); // 二级暂存值
-  const [expModalVisible, setExpModalVisible] = useState(false); // 弹窗是否显示
   const { actionVariables, routeOptions } = useWorkspaceData();
   const workspace = useWorkspace();
   const modalOptions = workspace.activeViewModule.listModals() || [];
@@ -61,15 +51,37 @@ export function EventSetter(props: EventSetterProps) {
     },
     [onChange, code],
   );
+  const options = useMemo(
+    () => [
+      { label: '无动作', value: EventAction.NoAction },
+      { label: '打印事件', value: EventAction.ConsoleLog },
+      {
+        label: (
+          <ExpressionPopover
+            title={modalTitle}
+            value={value}
+            onOk={(nextValue) => {
+              handleChange(nextValue);
+            }}
+            dataSource={actionVariables}
+          >
+            <Text>绑定 JS 表达式</Text>
+          </ExpressionPopover>
+        ),
+        value: EventAction.BindExpression,
+      },
+      { label: '打开页面', value: EventAction.NavigateTo },
+      { label: '打开弹窗', value: EventAction.OpenModal },
+      { label: '关闭弹窗', value: EventAction.CloseModal },
+    ],
+    [modalTitle, value, actionVariables, handleChange],
+  );
 
   const onAction = (key: string) => {
     setType(key as EventAction); // 记录事件类型
     setTemp(''); // 重置二级选项值
 
     switch (key) {
-      case EventAction.BindExpression:
-        setExpModalVisible(true);
-        break;
       case EventAction.ConsoleLog:
         handleChange('(...args) => console.log(...args)');
         break;
@@ -86,17 +98,6 @@ export function EventSetter(props: EventSetterProps) {
   return (
     <Box css={wrapperStyle}>
       <ActionSelect options={options} onSelect={onAction} text={actionText} />
-      <ExpressionModal
-        title={modalTitle}
-        value={code}
-        visible={expModalVisible}
-        onCancel={() => setExpModalVisible(false)}
-        onOk={(nextValue) => {
-          setExpModalVisible(false);
-          handleChange(nextValue);
-        }}
-        dataSource={actionVariables}
-      />
       {type === EventAction.NavigateTo && (
         <AutoComplete
           placeholder="选择或输入页面路由"
